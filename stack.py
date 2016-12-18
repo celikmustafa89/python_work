c7d38dd41e4e59ed95e6d2075b4525b6c28eb908
Mustafa ÇELİK <celikmustafa89@gmail.com>
2016-12-18 13:53:50 +0200

    Stack implementation is done

---
+++ stack.py
@@ -0,0 +1,43 @@
+__author__ = 'Mustafa ÇELİK'
+
+
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return self.items == []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[len(self.items)-1]
+
+    def size(self):
+        return len(self.items)
+
+    def clear(self):
+        self.items = []
+
+    def __str__(self):
+        return str(self.items)
+
+if __name__ == "__main__":
+
+    s = Stack()
+    print(s.is_empty())
+    s.push(4)
+    s.push('dog')
+    print(s.peek())
+    s.push(True)
+    print(s.size())
+    print(s.is_empty())
+    s.push(8.4)
+    print(s)
+    print(s.pop())
+    print(s.pop())
+
